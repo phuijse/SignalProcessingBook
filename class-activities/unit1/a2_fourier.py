eba04387b0b6439f3203395a3dbaf8ef466c9548
Pablo Huijse <pablo.huijse@gmail.com>
2021-08-23 10:48:17 -0400

    activities 1 and 2

---
+++ class-activities/unit1/a2_fourier.py
@@ -0,0 +1,20 @@
+
+"""
+
+# Actividad en clases: FFT
+ 
+- Demostración de `scipy.fft`
+- Espectro de magnitud y espectro de fase/ángulo
+- Espectro de ruido blanco y ruido rojo
+
+"""
+
+import numpy as np
+from IPython import get_ipython
+from IPython.display import Audio
+get_ipython().run_line_magic('matplotlib', 'ipympl')
+import matplotlib.pyplot as plt
+
+
+
+
